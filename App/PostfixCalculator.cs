a062e7a979633bc68c3713b39453c09f2c3ac562
ArtyomTsiganov <tsiganovartem06@gmail.com>
2024-11-06 18:44:17 +0500

    done

--- App/PostfixCalculator.cs
+++ App/PostfixCalculator.cs
@@ -8,7 +8,41 @@ namespace App
     {
         public static string Calculate(string postfixExpression)
         {
-            throw new NotImplementedException();
+            if (postfixExpression == null) throw new FormatException();
+            if (postfixExpression == "") return "0";
+            var postfix = postfixExpression.Split(' ');
+            var stack = new Stack<LongComplex>();
+            foreach (var sym in postfix)
+            {
+                if (sym == "+")
+                {   
+                    if (stack.Count < 2) throw new FormatException();
+                    var x = stack.Pop();
+                    var y = stack.Pop();
+                    stack.Push(LongComplex.Add(y, x));
+                }
+                else if (sym == "-")
+                {
+                    if (stack.Count < 2) throw new FormatException();
+                    var x = stack.Pop();
+                    var y = stack.Pop();
+                    stack.Push(LongComplex.Subtract(y, x));
+                }
+                else if (sym == "*")
+                {
+                    if (stack.Count < 2) throw new FormatException();
+                    var x = stack.Pop();
+                    var y = stack.Pop();
+                    stack.Push(LongComplex.Multiply(y, x));
+                }
+                else
+                {
+                    stack.Push(LongComplex.Parse(sym));
+                }
+            }
+            var result = stack.Pop().ToString();
+            if (stack.Count > 0) throw new FormatException();
+            return result;
         }
     }
 }
